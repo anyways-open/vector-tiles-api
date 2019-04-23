f8f4d31ba356cc3e1f497bcc6328d8080de3416e
Pieter Vander Vennet <pietervdvn@gmail.com>
2019-04-23 11:46:15 +0200

    Fix typo

--- deployment/update-vector-tiles.sh
+++ deployment/update-vector-tiles.sh
@@ -7,4 +7,4 @@ echo "Running vector tile generation"
 
 
 docker pull anywaysopen/vector-tiles-generator:latest
-docker run --rm -v /var/data/mapdata/:/var/app/source/ -v /var/services/vector-tiles-api/data/cyclenetworks-test/:/var/app/tiles/ anyways-open/vector-tiles-generator:latest
+docker run --rm -v /var/data/mapdata/:/var/app/source/ -v /var/services/vector-tiles-api/data/cyclenetworks-test/:/var/app/tiles/ anywaysopen/vector-tiles-generator:latest
