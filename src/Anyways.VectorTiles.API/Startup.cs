a20f8eac93cfb69a66e8353f770d46fb3265e1f2
xivk <ben@itinero.tech>
2019-12-12 13:22:56 +0100

    Cors is for nginx.

--- src/Anyways.VectorTiles.API/Startup.cs
+++ src/Anyways.VectorTiles.API/Startup.cs
@@ -23,13 +23,7 @@ namespace Anyways.VectorTiles.API
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddCors(options =>
-            {
-                options.AddPolicy("AllowAllOrigins",
-                    builder => builder.AllowAnyOrigin());
-            });
-            
+        {            
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
@@ -78,9 +72,6 @@ namespace Anyways.VectorTiles.API
                 return next();
             });
             
-            app.UseCors("AllowAllOrigins");
-
-            app.UseHttpsRedirection();
             app.UseMvc();
             
             DataPath = Configuration["data"];
